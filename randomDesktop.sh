8c569b4875ce6510f861399583747e80c86014d2
Jordan Hershberger <jojohersh@gmail.com>
2020-05-10 19:19:43 -0400

    created shell script to change the desktop.

--- randomDesktop.sh
+++ randomDesktop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gsettings set org.gnome.desktop.background picture-uri file://$(pwd)/images/$((RANDOM % 665)).jpg
+gsettings set org.gnome.desktop.background picture-uri file:///home/jordan/Documents/code/wallpaperChanger/images/$((RANDOM % 665)).jpg
