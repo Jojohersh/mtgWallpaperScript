40931affcbde20421edb81486d86d9313ac7ee0d
Jordan Hershberger <jojohersh@gmail.com>
2020-05-10 00:04:59 -0400

    able to retrieve api call and write image uris to file for a single page.

--- app.js
+++ app.js
@@ -8,20 +8,26 @@ var query = "?q=";
 query += "t:land";
 url += query;
 
-// var data = requestPage(url).then((data)=>console.log(data.total_cards));
 processCards(url);
 
 async function processCards(url) {
-  do {
+  // do {
     var page = await requestPage(url);
+    var cards = page.data;
+    writeCardsToFile(cards, "card_image_uris.txt");
+    // console.log(cards);
+    // diagnostics *********************************************
     // printCards(page);
     console.log(`first card of page: ${page.data[0].name}`);
+    console.log(`Next page: ${page.next_page}`);
+    //**********************************************************
     url = page.next_page;
-    console.log(`Next page: ${url}`);
 
-  } while(page.has_more);
+  // } while(page.has_more);
 }
 
+// takes api scryfall api url as input
+// returns a Promise<JSON> of the api response
 async function requestPage(url) {
   // fetch data from the scryfall api
   //fetch returns a Promise<Response>
@@ -30,24 +36,28 @@ async function requestPage(url) {
   return page = await response.json();
 }
 
-// function requestPage_old(url) {
-//   request(url, (err, res, body) =>{
-//     if(!err && res.statusCode == 200) {
-//       var cards = JSON.parse(body);
-//       var numCards = cards.total_cards;
-//       // scryfall limits each api call to 175 entries per page
-//       var numPages = Math.ceil(numCards / 175);
-//       console.log( `Number of pages: ${numPages}`);
-//       console.log(`Total land cards: ${cards.total_cards}`);
-//     }
-//   });
-// }
-
+//input should be an array of cards, derived from page.data
+//prints the image uri of every card
 function printCards(cards) {
   // Spit out the full image uri for each card
-  cards.data.forEach((card, index)=>{
+  cards.forEach((card, index)=>{
     if(card.image_uris && card.image_uris.art_crop) {
       console.log(`${index}: ${card.image_uris.art_crop}`);
+    }
+  });
+}
+
+//takes 'cards', an array of cards derived from page.data
+//      'fileName', the file to be written to
+// appends a new line consisting of the card's image
+function writeCardsToFile(cards, fileName) {
+  cards.forEach((card)=>{
+    if(card.image_uris && card.image_uris.art_crop) {
+      var cardImgUri = `${card.image_uris.art_crop}\n`;
+      fs.appendFile(fileName,cardImgUri,(err)=>{
+        if (err) console.error(`yikes, something went wrong processing ${card.name}`);
+        console.log(`${card.name} written`);
+      })
     }
   });
 }
