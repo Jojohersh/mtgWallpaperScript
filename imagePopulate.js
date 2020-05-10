1d7d8bec3d452190545e90c7dc64017d33cbaa9e
Jordan Hershberger <jojohersh@gmail.com>
2020-05-10 02:20:40 -0400

    renamed app.js to imagePopulate.js to make clearer what it does. Also started the bash script for randomly changing wallpaper. Images are not included in the repo.

--- imagePopulate.js
+++ imagePopulate.js
@@ -2,31 +2,39 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
 
+//define the search query
 var url = "https://api.scryfall.com/cards/search";
 var query = "?q=";
-//search for land cards
+//search query for land cards
 query += "t:land";
 url += query;
 
+// do the main storage actions for query results
 processCards(url);
 
+//************************************************
+// FUNCTION DECLARATIONS
 
 // takes a scryfall api url
 // extracts all image uris for the cards and stores into a text file
 async function processCards(url) {
+  // start requesting as many pages as needed
+  // note: scryfall limits 175 results per page
+
+  // index value of currently viewed card. is an object for pass through reference.
+  var cardIndex = {value:0};
   do {
+    console.log("processing...");
     var page = await requestPage(url);
     var cards = page.data;
+    //save all uris to a txt file
     writeCardsToFile(cards, "card_image_uris.txt");
-    // console.log(cards);
-    // diagnostics *********************************************
-    // printCards(page);
-    console.log(`first card of page: ${page.data[0].name}`);
-    console.log(`Next page: ${page.next_page}`);
-    //**********************************************************
+    //save all the images
+    saveCardImages(cards,cardIndex);
+    // update url to the next page of values
     url = page.next_page;
-
   } while(page.has_more);
+  console.log("Finished.");
 }
 
 // takes api scryfall api url as input
@@ -59,8 +67,29 @@ function writeCardsToFile(cards, fileName) {
       var cardImgUri = `${card.image_uris.art_crop}\n`;
       fs.appendFile(fileName,cardImgUri,(err)=>{
         if (err) console.error(`yikes, something went wrong processing ${card.name}`);
-        console.log(`${card.name} written`);
+        // console.log(`${card.name} written`);
       })
     }
   });
+}
+
+// takes an array of cards derived from page.data
+//       an index value for which total card it is on
+function saveCardImages(cards,cardIndex) {
+  cards.forEach((card)=>{
+    if(card.image_uris && card.image_uris.art_crop){
+      uri = card.image_uris.art_crop;
+      saveCardImage(uri, `./images/${cardIndex.value}.jpg`);
+      cardIndex.value++;
+    }
+  });
+}
+
+//takes an image uri as an input
+//      a filename to be saved to
+// downloads the image
+async function saveCardImage(uri, fileName) {
+  var response = await fetch(uri);
+  const dest = fs.createWriteStream(fileName);
+  response.body.pipe(dest);
 }
